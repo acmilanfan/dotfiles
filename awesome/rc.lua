031f267c0c3ff4b981156775f99ce14612f44dc6
Andrei Shumailov <gentoo1993@gmail.com>
2020-05-03 10:26:57 +0200

    awesome wm no border for floating or singel window

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -830,6 +830,21 @@ client.connect_signal("request::titlebars", function(c)
     }
 end)
 
+
+-- Hide border for floating or not tiling windows
+screen.connect_signal("arrange", function (s)
+    local max = s.selected_tag.layout.name == "max"
+    local only_one = #s.tiled_clients == 1 -- use tiled_clients so that other floating windows don't affect the count
+    -- but iterate over clients instead of tiled_clients as tiled_clients doesn't include maximized windows
+    for _, c in pairs(s.clients) do
+        if (max or only_one) and not c.floating or c.maximized then
+            c.border_width = 0
+        else
+            c.border_width = beautiful.border_width
+        end
+    end
+end)
+
 -- Enable sloppy focus, so that focus follows mouse.
 client.connect_signal("mouse::enter", function(c)
     c:emit_signal("request::activate", "mouse_enter", { raise = false })
