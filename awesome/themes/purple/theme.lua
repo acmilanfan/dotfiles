0bc34cf39d568ae93235e02a31507045ea81626b
Andrei Shumailov <gentoo1993@gmail.com>
2020-05-25 12:48:42 +0200

    Adjust colours

--- awesome/themes/purple/theme.lua
+++ awesome/themes/purple/theme.lua
@@ -17,7 +17,8 @@ local theme = dofile(themes_path.."default/theme.lua")
 theme.font          = "Roboto Medium 9"
 
 theme.bg_normal     = xrdb.background
-theme.bg_focus      = xrdb.color12
+-- theme.bg_focus      = "#734899"
+theme.bg_focus      = "#8a2be2"
 theme.bg_urgent     = xrdb.color9
 theme.bg_minimize   = xrdb.color8
 theme.bg_systray    = theme.bg_normal
